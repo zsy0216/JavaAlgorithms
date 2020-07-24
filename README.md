fcd59aa2a140abd1a989a9ee1658666eff96e788
zsy0216 <594983498@qq.com>
2020-07-24 10:44:47 +0800

    binary search & project init

---
+++ README.md
@@ -0,0 +1,8 @@
+# JavaAlgorithms Java 算法
+
+> 参考：
+>
+> [TheAlgorithms&Java](https://github.com/TheAlgorithms/Java) 
+>
+> 《算法》 第四版 Robert Sedgewick & Kevin Wayne 著 谢路云 译
+
---
+++ src/com/tassel/search/BinarySearch.java
@@ -0,0 +1,50 @@
+package com.tassel.search;
+
+import java.util.Arrays;
+
+/**
+ * @author shuaiyin.zhang
+ * @description 二分查找算法
+ * @date 2020/07/24
+ */
+public class BinarySearch {
+	/**
+	 * 本例来自 《算法》第四版 1.1.10
+	 * 数组必须是有序的
+	 */
+	public static int rank(int target, int[] a) {
+		int lo = 0;
+		int hi = a.length - 1;
+		while (lo <= hi) {
+			int mid = lo + (hi - lo) / 2;
+			if (target < a[mid]) {
+				// 目标在左半部分
+				hi = mid - 1;
+			} else if (target > a[mid]) {
+				// 目标在右半部分
+				lo = mid + 1;
+			} else {
+				return mid;
+			}
+		}
+		return -1;
+	}
+
+	/**
+	 * test
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int[] a = {1, 3, 4, 7, 13, 35, 44, 89};
+		int target = 13;
+		// 保证有序
+		Arrays.sort(a);
+		int t = rank(target, a);
+		if (t == -1) {
+			System.out.println(target + " 不在数组内. ");
+		} else {
+			System.out.println(target + " 在数组索引为 " + t + " 的位置. ");
+		}
+	}
+}
